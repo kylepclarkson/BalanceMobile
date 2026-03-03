294f8ab5b61e08f577bb1871bcb82e22a990be0a
Kyle Clarkson <kyleclarkson17@hotmail.com>
2026-03-02 22:16:44 -0800

    Cleared out initial example code.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,5 @@
-import '../global.css';
+import { Stack } from "expo-router";
 
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
